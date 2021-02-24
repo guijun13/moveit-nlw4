d0433409d16fe1d39e494276a8a2e1d6c9817122
guijun13 <guilhermejun13.grillo@gmail.com>
2021-02-24 00:40:57 -0300

    :recycle: Add 'ExperienceBar' component && global style
    Reused component and style from #1 class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import ExperienceBar from '../components/ExperienceBar';
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
 }
