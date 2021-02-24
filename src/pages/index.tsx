1d3e32fdd57201e17eb2fbb47ad9dca8ab6daaad
guijun13 <guilhermejun13.grillo@gmail.com>
2021-02-24 01:11:37 -0300

    :sparkles: Add 'CompletedChallenges' component on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CompletedChallenges from '../components/CompletedChallenges';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
 
@@ -12,6 +13,7 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
         </div>
         <div></div>
       </section>
