80f23931cdda6d36580c9dc650b433cfdf01da33
guijun13 <guilhermejun13.grillo@gmail.com>
2021-02-24 01:04:52 -0300

    :sparkles: Add 'Profile' component on homepage

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import styles from '../styles/components/ExperienceBar.module.css';
 
 export default function ExperienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{ width: '50%' }}>
           <span
-            className="current-experience"
+            className={styles.currentExperience}
             style={{
               left: '50%',
             }}
