71637a5d86fd68039a91c0ffa234df6eff4fe2b1
guijun13 <guilhermejun13.grillo@gmail.com>
2021-02-25 02:26:53 -0300

    :sparkles: Add 'startNewChallenge()' on '<ChallengeBox/>'
    Reveal a new random challenge called from .json file when the countdown ends

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,9 +1,16 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
 
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
 }
@@ -18,18 +25,28 @@ export default function ChallengesProvider({ children }: ChallengesProviderProps
   const [level, setLevel] = useState(1);
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('New challenge');
+    const randomChallengesIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengesIndex];
+    setActiveChallenge(challenge);
   }
 
   return (
     <ChallengesContext.Provider
-      value={{ level, currentExperience, challengesCompleted, levelUp, startNewChallenge }}
+      value={{
+        level,
+        currentExperience,
+        challengesCompleted,
+        activeChallenge,
+        levelUp,
+        startNewChallenge,
+      }}
     >
       {children}
     </ChallengesContext.Provider>
