a0582283461fbffc9f3b10932d3548825f0a0f7f
guijun13 <guilhermejun13.grillo@gmail.com>
2021-02-25 00:59:28 -0300

    :sparkles: Add static 'ChallengeBox' component on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
 
 import styles from '../styles/pages/Home.module.css';
+import ChallengeBox from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -21,7 +22,9 @@ export default function Home() {
           <CompletedChallenges />
           <Coutdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
